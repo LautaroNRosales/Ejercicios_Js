4c9cd402bf22c04d9b1338668ec4e245528610de
LautaroNRosales <lautarounraf@gmail.com>
2024-04-21 17:52:06 -0300

    Empezamos con el 5

--- Ejercicio4.js
+++ Ejercicio4.js
@@ -1,1 +1,4 @@
 //Ingresar un número e indicar si es par.
+let num = 24
+ if (num % 2 == 0)
+console.log(`El numero ${num} es par`)
---
+++ Ejercicio5.js
@@ -0,0 +1,1 @@
+//Ingresar una palabra y un numero "n". Repetir la palabra "n" veces.
