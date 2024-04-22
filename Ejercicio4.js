d5122628c8b7059b088dad3a6266e24e3c9f2e7b
LautaroNRosales <lautarounraf@gmail.com>
2024-04-21 21:20:53 -0300

    Modificamos el 4

--- Ejercicio4.js
+++ Ejercicio4.js
@@ -1,4 +1,5 @@
 //Ingresar un número e indicar si es par.
 let num = 24
- if (num % 2 == 0)
-console.log(`El numero ${num} es par`)
+ if (num % 2 == 0) {
+    console.log(`El numero ${num} es par`)
+ }
