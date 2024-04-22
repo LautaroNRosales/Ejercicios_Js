e31edaf7f309926e13581c30bba4a44e93f01e15
LautaroNRosales <lautarounraf@gmail.com>
2024-04-21 22:39:08 -0300

    Terminamos el 6

--- Ejercicio5.js
+++ Ejercicio5.js
@@ -1,5 +1,5 @@
 //Ingresar una palabra y un numero "n". Repetir la palabra "n" veces.
-
+let pal = "Chasquido"
 for ( i = 0; i < 5; i++ ) {
-    console.log("Chasquido")
+    console.log(pal)
 }
---
+++ Ejercicio6.js
@@ -0,0 +1,16 @@
+//Ingresar dos valores enteros y enumerar los elementos que los separan.
+let n1 = 20
+let n2 = 25
+let n3 = 50
+let n4 = 45
+if (n1 <= n2) {
+    for ( i = 21; i < 25; i++ )
+        console.log(i)
+}
+
+console.log()
+
+if (n3 >= n4) {
+    for ( i = 49; i > 45; i-- )
+    console.log(i)
+}
